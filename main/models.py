b7b9f3db801eb010082152b105dddbbafed7d723
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2022-10-10 16:59:39 +0500

    created table WeatherStatus

--- main/models.py
+++ main/models.py
@@ -6,3 +6,7 @@ class City(models.Model):
     def __str__(self):
         return self.city_name
 
+class WeatherStatus(models.Model):
+    precipitation = models.CharField(max_length=25)
+    cloud_cover = models.CharField(max_length=25)
+    cloud_img = models.CharField(max_length=25)
