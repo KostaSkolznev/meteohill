0e524f39326ac6089cb6eaaa789a32e1ca7b190f
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2022-10-11 12:09:27 +0500

    create WeatherService table

--- main/models.py
+++ main/models.py
@@ -10,3 +10,11 @@ class WeatherStatus(models.Model):
     precipitation = models.CharField(max_length=25)
     cloud_cover = models.CharField(max_length=25)
     cloud_img = models.CharField(max_length=25)
+
+class WeatherService(models.Model):
+    service_name = models.CharField(max_length=25)
+    service_url = models.CharField(max_length=250)
+    api_url = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.service_name
