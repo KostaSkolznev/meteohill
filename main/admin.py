16325e27e1ca96da273e6bf786060445d831a5d0
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2022-10-12 12:14:28 +0500

    added models to admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import City
+from .models import WeatherStatus
+from .models import WeatherService
+from .models import Weather
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(WeatherStatus)
+admin.site.register(WeatherService)
+admin.site.register(Weather)
