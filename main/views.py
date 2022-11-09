613f7b16fe57cc2162b326685a2da4995ce7e38f
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2022-11-09 16:45:42 +0500

    move code with data from views to weatherdata

--- main/views.py
+++ main/views.py
@@ -2,16 +2,11 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
+from main.weatherdata import weather_info
 
 def index(request):
     url = 'http://dataservice.accuweather.com/currentconditions/v1/295863?apikey=ICseysmELVRIU8R8EF1lle8lR88uJgX6'
     res = requests.get(url).json()
-
-    weather_info = {
-        'temp': round(res[0]["Temperature"]["Metric"]["Value"]),
-        'precipitation': res[0]["PrecipitationType"],
-        'clouds': res[0]["WeatherText"]
-    }
 
     context = {
         'info': weather_info
@@ -20,18 +15,6 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def accuweather(request):
-    url = 'http://dataservice.accuweather.com/currentconditions/v1/295863?apikey=ICseysmELVRIU8R8EF1lle8lR88uJgX6&details=true'
-    res = requests.get(url).json()
-
-    weather_info = {
-        'temp': round(res[0]["Temperature"]["Metric"]["Value"]),
-        'tempnight': round(res[0]["TemperatureSummary"]["Past24HourRange"]["Minimum"]["Metric"]["Value"]),
-        'humidity': res[0]["RelativeHumidity"],
-        'precipitation': res[0]["PrecipitationType"],
-        'clouds': res[0]["WeatherText"],
-        'wind': res[0]["Wind"]["Speed"]["Metric"]["Value"],
-        'windgust': res[0]["WindGust"]["Speed"]["Metric"]["Value"],
-    }
 
     context = {
         'info': weather_info
---
+++ main/weatherdata.py
@@ -0,0 +1,16 @@
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse
+
+url = 'http://dataservice.accuweather.com/currentconditions/v1/295863?apikey=ICseysmELVRIU8R8EF1lle8lR88uJgX6&details=true'
+res = requests.get(url).json()
+
+weather_info = {
+    'temp': round(res[0]["Temperature"]["Metric"]["Value"]),
+    'tempnight': round(res[0]["TemperatureSummary"]["Past24HourRange"]["Minimum"]["Metric"]["Value"]),
+    'humidity': res[0]["RelativeHumidity"],
+    'precipitation': res[0]["PrecipitationType"],
+    'clouds': res[0]["WeatherText"],
+    'wind': res[0]["Wind"]["Speed"]["Metric"]["Value"],
+    'windgust': res[0]["WindGust"]["Speed"]["Metric"]["Value"],
+}
