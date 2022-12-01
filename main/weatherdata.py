0192a1b2896281cf4161f19a2cb23239333819e1
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2022-12-01 10:00:07 +0500

    logical changes: if data on this day exist than read from DB or AccuWeather API and add data to database

--- main/weatherdata.py
+++ main/weatherdata.py
@@ -12,15 +12,39 @@ checkdate = Weather.objects.filter(date__date=now, service=1)
 
 if checkdate.exists():
     a = True
-else: a = False
+    weather_info = {
+        'temp': checkdate.temperature_day,
+        'tempnight': checkdate.temperature_night,
+        'humidity': checkdate.humidity,
+        'precipitation': checkdate.precipitation_type,
+        'clouds': checkdate.precipitation_type,
+        'wind': checkdate.wind,
+        'windgust': checkdate.windgusts,
+        'datenow': a,
+    }
 
-weather_info = {
-    'temp': round(res[0]["Temperature"]["Metric"]["Value"]),
-    'tempnight': round(res[0]["TemperatureSummary"]["Past24HourRange"]["Minimum"]["Metric"]["Value"]),
-    'humidity': res[0]["RelativeHumidity"],
-    'precipitation': res[0]["PrecipitationType"],
-    'clouds': res[0]["WeatherText"],
-    'wind': res[0]["Wind"]["Speed"]["Metric"]["Value"],
-    'windgust': res[0]["WindGust"]["Speed"]["Metric"]["Value"],
-    'datenow': a,
-}
+else:
+    a = False
+    weather_info = {
+        'temp': round(res[0]["Temperature"]["Metric"]["Value"]),
+        'tempnight': round(res[0]["TemperatureSummary"]["Past24HourRange"]["Minimum"]["Metric"]["Value"]),
+        'humidity': res[0]["RelativeHumidity"],
+        'precipitation': res[0]["PrecipitationType"],
+        'clouds': res[0]["WeatherText"],
+        'wind': res[0]["Wind"]["Speed"]["Metric"]["Value"],
+        'windgust': res[0]["WindGust"]["Speed"]["Metric"]["Value"],
+        'datenow': a,
+    }
+
+    add_weather = Weather(
+        city = 1,
+        service = 1,
+        humidity = {{ weather_info.humidity }},
+        wind = {{ weather_info.wind }},
+        windgusts = {{ weather_info.windgust }},
+        temperature_day = {{ weather_info.temp }},
+        temperature_night = {{weather_info.tempnight}},
+        precipitation_type = 1,
+    )
+
+    add_weather.save()
