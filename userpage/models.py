40c38393a2d3094e358305ece0600e7524cb0407
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2023-05-03 15:46:54 +0500

    added userpage, this is page for user profile. added model in DB for userpage options.

--- userpage/models.py
+++ userpage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from main.models import City
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    SelectedCity = models.ForeignKey(City, on_delete=models.SET_DEFAULT, default = 1)
+    UserID = models.OneToOneField(User, on_delete=models.CASCADE)
