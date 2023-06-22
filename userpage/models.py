b92e77929e8ac72353d1650b7fc4984ea357aead
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2023-06-22 13:25:21 +0500

    added row naming by username

--- userpage/models.py
+++ userpage/models.py
@@ -4,4 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     SelectedCity = models.ForeignKey(City, on_delete=models.SET_DEFAULT, default = 1)
-    UserID = models.OneToOneField(User, on_delete=models.CASCADE)
+    UserID = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.UserID.username
