aea65646f7f5dd5a116eb372df9e2225d2865f94
Kosta Skolznev <44146264+KostaSkolznev@users.noreply.github.com>
2023-08-08 15:53:21 +0500

    template for file localsettings.py

---
+++ meteohill/localsettings_template.py
@@ -0,0 +1,4 @@
+#local settings file, put here your tokens for auth in weather services
+#this is template for localsettings.py rename this file or copy&rename to localsettings.py
+accuweather_token = 'put here token accuweather'
+openweather_token = 'put here token openweather'
